250de3bc5dca8d3dcc2b0d40a0bfd33eb1ab2de8
Matias Lang <matiaslang@icloud.com>
2020-11-27 00:00:39 +0200

    commit to test the tunnel

--- src/App.js
+++ src/App.js
@@ -1,6 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
 
+const Timer = () => {
+  let year = new Date().getFullYear();
+
+  return(
+    <div>
+    <p>
+      ps. these pages should be finished by the end of the year {year}
+    </p>
+    </div>
+  )
+}
+
 function App() {
   return (
     <div className="App">
@@ -11,6 +23,7 @@ function App() {
         <p>
           I will be adding more information about me to this page in the near future.
         </p>
+        <Timer/>
         <p>-Matias Lang</p>
       </header>
     </div>
