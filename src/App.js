70bf2f98390053fe095a3413a8256d0aef06686c
Matias Lang <matiaslang@icloud.com>
2021-07-02 01:28:14 +0300

    first version of routing done

--- src/App.js
+++ src/App.js
@@ -1,33 +1,137 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment } from 'react'
+import styled from 'styled-components'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  useRouteMatch,
+  useParams,
+  useHistory,
+  Redirect,
+  withRouter,
+  BrowserRouter,
+} from 'react-router-dom'
+import './App.css'
+import Tabs from '@material-ui/core/Tabs'
+import Tab from '@material-ui/core/Tab'
+import Paper from '@material-ui/core/Paper'
 
-const Timer = () => {
-  let year = new Date().getFullYear();
+const menuButton = styled.button``
+const Wrapper = styled.section`
+  background-color: papayawhip;
+`
 
-  return(
+const App = () => {
+  const allTabs = ['/', '/memorygame', '/nothing']
+  let history = useHistory()
+  const handleChange = (event, value) => {
+    console.log('Redirecting to:')
+    console.log(value)
+    return
+  }
+
+  return (
     <div>
-    <p>
-      ps. these pages should be finished by the end of the year {year}
-    </p>
+      <BrowserRouter>
+        <Wrapper>
+          <Route
+            path='/'
+            render={({ location }) => (
+              <Fragment>
+                <Paper className={'ClassName'}>
+                  <Tabs
+                    value={location.pathname}
+                    indicatorColor='secondary'
+                    variant='fullWidth'
+                    textColor='primary'
+                    centered
+                  >
+                    <Tab
+                      label='Home'
+                      value='/home'
+                      component={Link}
+                      to={allTabs[0]}
+                    />
+                    <Tab
+                      label='Memory Game'
+                      value='/memorygame'
+                      component={Link}
+                      to={allTabs[1]}
+                    />
+                    <Tab
+                      label='Noothiiing....'
+                      value='/nothing'
+                      disableRipple
+                      component={Link}
+                      to={allTabs[2]}
+                    />
+                  </Tabs>
+                </Paper>
+
+                <Switch>
+                  <Route path={allTabs[0]}>
+                    <About />
+                  </Route>
+                  <Route path={allTabs[1]}>
+                    <Topics />
+                  </Route>
+                  <Route path={allTabs[2]}>
+                    <Home />
+                  </Route>
+                </Switch>
+              </Fragment>
+            )}
+          />
+        </Wrapper>
+      </BrowserRouter>
     </div>
   )
 }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Hello to my personal website!
-        </p>
-        <p>
-          I will be adding more information about me to this page in the near future.
-        </p>
-        <Timer/>
-        <p>-Matias Lang</p>
-      </header>
-    </div>
-  );
+function Home() {
+  return <h2>Home</h2>
 }
 
-export default App;
+function About() {
+  return <h2>1</h2>
+}
+
+function Topics() {
+  let match = useRouteMatch()
+
+  return (
+    <div>
+      <h2>Topics</h2>
+
+      <ul>
+        <li>
+          <Link to={`${match.url}/components`}>Components</Link>
+        </li>
+        <li>
+          <Link to={`${match.url}/props-v-state`}>Props v. State</Link>
+        </li>
+      </ul>
+
+      {/* The Topics page has its own <Switch> with more routes
+          that build on the /topics URL path. You can think of the
+          2nd <Route> here as an "index" page for all topics, or
+          the page that is shown when no topic is selected */}
+      <Switch>
+        <Route path={`${match.path}/:topicId`}>
+          <Topic />
+        </Route>
+        <Route path={match.path}>
+          <h3>Please select a topic.</h3>
+        </Route>
+      </Switch>
+    </div>
+  )
+}
+
+function Topic() {
+  let { topicId } = useParams()
+  return <h3>Requested topic ID: {topicId}</h3>
+}
+
+export default App
